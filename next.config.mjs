549f50856970005b0aa65149ccf515feaf60e76f
Karen <justusethis420@gmail.com>
2026-02-08 10:49:59 +0000

    fix: Refine Webpack config for WebAssembly

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    };
-    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+  webpack(config, { isServer, dev }) {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+    config.output.webassemblyModuleFilename =
+      isServer && !dev
+        ? '../static/wasm/[modulehash].wasm'
+        : 'static/wasm/[modulehash].wasm';
+
+    config.module.parser.javascript.wasmEnabled = true;
+
     return config;
   },
 };
