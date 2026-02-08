968fd923fc00a8ee53e63c83aac699c347a86dff
Karen <justusethis420@gmail.com>
2026-02-08 09:50:06 +0000

    Add android platform and workflow

---
+++ android/app/src/main/java/com/TheTrollVault/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.TheTrollVault.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.TheTrollVault.app',
+  appName: 'TheTrollVault',
+  webDir: 'out'
+};
+
+export default config;
